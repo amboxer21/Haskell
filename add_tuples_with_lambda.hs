8ad4778f10a0cebed8ea57f005c044d63408c951
Anthony Guevara <amboxer21@gmail.com>
2021-07-27 08:47:30 -0400

    Update add_tuples_with_lambda.hs

--- add_tuples_with_lambda.hs
+++ add_tuples_with_lambda.hs
@@ -1,5 +1,4 @@
 main :: IO ()
 main = do
+     -- (lambda x : [n[0]+n[1] for n in x]) ([(5,6),(5,9)]) Python equivelant
      print((\xs -> [ x+s | (x,s) <- xs ]) [(2,3), (5,6)])
-     -- Python equivelant
-     -- (lambda x : [n[0]+n[1] for n in x]) ([(5,6),(5,9)])
